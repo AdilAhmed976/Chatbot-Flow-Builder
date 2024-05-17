4bb02d3b0231fa7e6f28b5529488ca60ec045f22
AdilAhmed976 <adilahmedah976@gmail.com>
2024-05-17 21:14:04 +0530

    fix : UI Updated of the textMessage Block

--- src/Components/React-Flow/node-edge-types/Nodes/TextMessage.jsx
+++ src/Components/React-Flow/node-edge-types/Nodes/TextMessage.jsx
@@ -1,5 +1,6 @@
 import { useCallback } from "react";
 import { Handle, Position } from "reactflow";
+import MessageIcon from "../../../../icons/MessageIcon";
 
 const handleStyle = { left: 10 };
 
@@ -8,11 +9,32 @@ function TextMessage({ data, isConnectable }) {
     <div>
       <Handle
         type="target"
-        position={Position.Top}
+        position={Position.Left}
         isConnectable={isConnectable}
       />
-      <div className="max-w-60 border border-gray-900 rounded-lg p-2">
-        <label htmlFor="text">Text: {data.label}</label>
+      <Handle
+        type="source"
+        position={Position.Right}
+        isConnectable={isConnectable}
+      />
+      <div className="min-w-[200px] max-w-[400px] border border-gray-900 rounded-lg h-auto bg-white">
+        <div className="bg-green-200 w-full rounded-tl-lg rounded-tr-lg p-1 py-1 flex items-center justify-between gap-2">
+          <div className="flex items-center gap-2">
+            <MessageIcon className="text-gray-600" size={14} />
+            <text className="font-bold text-md">Send Message</text>
+          </div>
+          <div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/598px-WhatsApp_icon.png"
+              alt=""
+              srcset=""
+              className="h-4 w-4"
+            />
+          </div>
+        </div>
+        <div className="p-2">
+          <label htmlFor="text">{data.label}</label>
+        </div>
       </div>
     </div>
   );
